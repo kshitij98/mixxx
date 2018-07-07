f743d0db1d039781bebd0141f6d5b47d6984a545
Kshitij Gupta <kgupta119@gmail.com>
2018-07-07 07:47:18 +0530

    Consolidated Effect and EffectSlot
    - Effect and EffectSlot classes had no clear separation of responsibility
    - Had to keep the duplicated state in both the classes in sync and thus was more susceptible to bugs
    - Ensured a loaded effect slot to have a corresponding EngineEffect added to the engine
    
      NOTE: XML saving/loading of effects is broken and will be re-implemented in the new architecture

--- build/depends.py
+++ build/depends.py
@@ -758,7 +758,6 @@ class MixxxCore(Feature):
                    "effects/effectmanifest.cpp",
                    "effects/effectmanifestparameter.cpp",
 
-                   "effects/effect.cpp",
                    "effects/effectparameter.cpp",
 
                    "effects/effectchainslot.cpp",
