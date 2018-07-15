kshitij98/mixxx

commit fded4f8d5b483681f19bbd0943baa806c8c8aad7

Removed EffectChainManager layer
- Removed the superfluous class `EffectChainManager` whose functionality is now shifted to `EffectsManager`
- Updated EffectProcessor to fix the circular include problem during compilation

  NOTE: XML saving/loading of effects is broken and will be re-implemented in the new architecture

src/effects/effectsmanager.cpp
@@ -6,9 +6,9 @@
 #include <algorithm>
 
 #include "engine/effects/engineeffectsmanager.h"
-#include "effects/effectchainmanager.h"
 #include "effects/effectsbackend.h"
 #include "effects/effectslot.h"
+#include "effects/effectxmlelements.h"
 #include "engine/effects/engineeffect.h"
 #include "engine/effects/engineeffectrack.h"
 #include "engine/effects/engineeffectchain.h"
@@ -25,7 +25,7 @@ EffectsManager::EffectsManager(QObject* pParent, UserSettingsPointer pConfig,
                                ChannelHandleFactory* pChannelHandleFactory)
         : QObject(pParent),
           m_pChannelHandleFactory(pChannelHandleFactory),
-          m_pEffectChainManager(new EffectChainManager(pConfig, this)),
+          m_pConfig(pConfig),
           m_nextRequestId(0),
           m_pLoEqFreq(NULL),
           m_pHiEqFreq(NULL),
@@ -50,8 +50,7 @@ EffectsManager::~EffectsManager() {
     m_underDestruction = true;
 
     // NOTE(Kshitij) : Use new functions for saving XML files
-    // m_pEffectChainManager->saveEffectChains();
-    delete m_pEffectChainManager;
+    // saveEffectChains();
     // This must be done here, since the engineRacks are deleted via
     // the queue
     processEffectsResponses();
@@ -113,20 +112,76 @@ void EffectsManager::slotBackendRegisteredEffect(EffectManifestPointer pManifest
     m_pNumEffectsAvailable->forceSet(m_availableEffectManifests.size());
 }
 
-void EffectsManager::registerInputChannel(const ChannelHandleAndGroup& handle_group) {
-    m_pEffectChainManager->registerInputChannel(handle_group);
+// TODO(Kshitij) : This function is not being used. Remove
+void EffectsManager::addEffectChain(EffectChainSlotPointer pEffectChainSlot) {
+    if (pEffectChainSlot) {
+        m_effectChainSlots.append(pEffectChainSlot);
+    }
 }
 
-const QSet<ChannelHandleAndGroup>& EffectsManager::registeredInputChannels() const {
-    return m_pEffectChainManager->registeredInputChannels();
+// TODO(Kshitij) : This function is not being used. Remove
+void EffectsManager::removeEffectChain(EffectChainSlotPointer pEffectChainSlot) {
+    if (pEffectChainSlot) {
+        m_effectChainSlots.removeAll(pEffectChainSlot);
+    }
+}
+
+// TODO(Kshitij) : This function is not being used. Remove
+EffectChainSlotPointer EffectsManager::getNextEffectChain(EffectChainSlotPointer pEffectChainSlot) {
+    if (m_effectChainSlots.isEmpty())
+        return EffectChainSlotPointer();
+
+    if (!pEffectChainSlot) {
+        return m_effectChainSlots[0];
+    }
+
+    int indexOf = m_effectChainSlots.lastIndexOf(pEffectChainSlot);
+    if (indexOf == -1) {
+        qWarning() << debugString() << "WARNING: getNextEffectChain called for an unmanaged EffectChain";
+        return m_effectChainSlots[0];
+    }
+
+    return m_effectChainSlots[(indexOf + 1) % m_effectChainSlots.size()];
+}
+
+// TODO(Kshitij) : This function is not being used. Remove
+EffectChainSlotPointer EffectsManager::getPrevEffectChain(EffectChainSlotPointer pEffectChainSlot) {
+    if (m_effectChainSlots.isEmpty())
+        return EffectChainSlotPointer();
+
+    if (!pEffectChainSlot) {
+        return m_effectChainSlots[m_effectChainSlots.size()-1];
+    }
+
+    int indexOf = m_effectChainSlots.lastIndexOf(pEffectChainSlot);
+    if (indexOf == -1) {
+        qWarning() << debugString() << "WARNING: getPrevEffectChain called for an unmanaged EffectChain";
+        return m_effectChainSlots[m_effectChainSlots.size()-1];
+    }
+
+    return m_effectChainSlots[(indexOf - 1 + m_effectChainSlots.size()) % m_effectChainSlots.size()];
+}
+
+bool EffectsManager::isAdoptMetaknobValueEnabled() const {
+    return m_pConfig->getValue(ConfigKey("[Effects]", "AdoptMetaknobValue"), true);
+}
+
+void EffectsManager::registerInputChannel(const ChannelHandleAndGroup& handle_group) {
+    VERIFY_OR_DEBUG_ASSERT(!m_registeredInputChannels.contains(handle_group)) {
+        return;
+    }
+    m_registeredInputChannels.insert(handle_group);
+
+    for (auto& pRack : m_standardEffectRacks) {
+        pRack->registerInputChannel(handle_group);
+    }
 }
 
 void EffectsManager::registerOutputChannel(const ChannelHandleAndGroup& handle_group) {
-    m_pEffectChainManager->registerOutputChannel(handle_group);
-}
-
-const QSet<ChannelHandleAndGroup>& EffectsManager::registeredOutputChannels() const {
-    return m_pEffectChainManager->registeredOutputChannels();
+    VERIFY_OR_DEBUG_ASSERT(!m_registeredOutputChannels.contains(handle_group)) {
+        return;
+    }
+    m_registeredOutputChannels.insert(handle_group);
 }
 
 const QList<EffectManifestPointer> EffectsManager::getAvailableEffectManifestsFiltered(
@@ -219,39 +274,63 @@ EffectPointer EffectsManager::instantiateEffect(const QString& effectId) {
 }
 
 StandardEffectRackPointer EffectsManager::addStandardEffectRack() {
-    return m_pEffectChainManager->addStandardEffectRack();
+    StandardEffectRackPointer pRack(new StandardEffectRack(
+        this, m_standardEffectRacks.size()));
+    m_standardEffectRacks.append(pRack);
+    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
+    return pRack;
 }
 
-StandardEffectRackPointer EffectsManager::getStandardEffectRack(int rack) {
-    return m_pEffectChainManager->getStandardEffectRack(rack);
+StandardEffectRackPointer EffectsManager::getStandardEffectRack(int i) {
+    if (i < 0 || i >= m_standardEffectRacks.size()) {
+        return StandardEffectRackPointer();
+    }
+    return m_standardEffectRacks[i];
 }
 
 EqualizerRackPointer EffectsManager::addEqualizerRack() {
-    return m_pEffectChainManager->addEqualizerRack();
+    EqualizerRackPointer pRack(new EqualizerRack(
+            this, m_equalizerEffectRacks.size()));
+    m_equalizerEffectRacks.append(pRack);
+    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
+    return pRack;
 }
 
-EqualizerRackPointer EffectsManager::getEqualizerRack(int rack) {
-    return m_pEffectChainManager->getEqualizerRack(rack);
+EqualizerRackPointer EffectsManager::getEqualizerRack(int i) {
+    if (i < 0 || i >= m_equalizerEffectRacks.size()) {
+        return EqualizerRackPointer();
+    }
+    return m_equalizerEffectRacks[i];
 }
 
 QuickEffectRackPointer EffectsManager::addQuickEffectRack() {
-    return m_pEffectChainManager->addQuickEffectRack();
+    QuickEffectRackPointer pRack(new QuickEffectRack(
+        this, m_quickEffectRacks.size()));
+    m_quickEffectRacks.append(pRack);
+    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
+    return pRack;
 }
 
-QuickEffectRackPointer EffectsManager::getQuickEffectRack(int rack) {
-    return m_pEffectChainManager->getQuickEffectRack(rack);
+QuickEffectRackPointer EffectsManager::getQuickEffectRack(int i) {
+    if (i < 0 || i >= m_quickEffectRacks.size()) {
+        return QuickEffectRackPointer();
+    }
+    return m_quickEffectRacks[i];
 }
 
 OutputEffectRackPointer EffectsManager::addOutputsEffectRack() {
-    return m_pEffectChainManager->addOutputsEffectRack();
+    OutputEffectRackPointer pRack(new OutputEffectRack(this));
+    m_pOutputEffectRack = pRack;
+    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
+    return m_pOutputEffectRack;
 }
 
 OutputEffectRackPointer EffectsManager::getOutputsEffectRack() {
-    return m_pEffectChainManager->getMasterEffectRack();
+    return m_pOutputEffectRack;
 }
 
 EffectRackPointer EffectsManager::getEffectRack(const QString& group) {
-    return m_pEffectChainManager->getEffectRack(group);
+    return m_effectRacksByGroup.value(group);
 }
 
 EffectSlotPointer EffectsManager::getEffectSlot(
@@ -359,8 +438,16 @@ void EffectsManager::setup() {
 //     m_pEffectChainManager->loadEffectChains();
 // }
 
-void EffectsManager::refeshAllRacks() {
-    m_pEffectChainManager->refeshAllRacks();
+void EffectsManager::refreshAllRacks() {
+    for (const auto& pRack: m_standardEffectRacks) {
+        pRack->refresh();
+    }
+    for (const auto& pRack: m_equalizerEffectRacks) {
+        pRack->refresh();
+    }
+    for (const auto& pRack: m_quickEffectRacks) {
+        pRack->refresh();
+    }
 }
 
 bool EffectsManager::writeRequest(EffectsRequest* request) {

build/depends.py
@@ -766,7 +766,6 @@ class MixxxCore(Feature):
                    "effects/effectparameterslot.cpp",
                    "effects/effectbuttonparameterslot.cpp",
                    "effects/effectsmanager.cpp",
-                   "effects/effectchainmanager.cpp",
                    "effects/effectsbackend.cpp",
 
                    "effects/builtin/builtinbackend.cpp",

src/effects/effectchainmanager.cpp
@@ -1,237 +1,0 @@
-#include "effects/effectchainmanager.h"
-#include "effects/effectsmanager.h"
-#include "effects/effectxmlelements.h"
-
-#include <QtDebug>
-#include <QDomDocument>
-#include <QFile>
-#include <QDir>
-
-EffectChainManager::EffectChainManager(UserSettingsPointer pConfig,
-                                       EffectsManager* pEffectsManager)
-        : QObject(pEffectsManager),
-          m_pConfig(pConfig),
-          m_pEffectsManager(pEffectsManager) {
-}
-
-EffectChainManager::~EffectChainManager() {
-    //qDebug() << debugString() << "destroyed";
-}
-
-void EffectChainManager::registerInputChannel(const ChannelHandleAndGroup& handle_group) {
-    VERIFY_OR_DEBUG_ASSERT(!m_registeredInputChannels.contains(handle_group)) {
-        return;
-    }
-    m_registeredInputChannels.insert(handle_group);
-
-    for (auto& pRack : m_standardEffectRacks) {
-        pRack->registerInputChannel(handle_group);
-    }
-}
-
-void EffectChainManager::registerOutputChannel(const ChannelHandleAndGroup& handle_group) {
-    VERIFY_OR_DEBUG_ASSERT(!m_registeredOutputChannels.contains(handle_group)) {
-        return;
-    }
-    m_registeredOutputChannels.insert(handle_group);
-}
-
-StandardEffectRackPointer EffectChainManager::addStandardEffectRack() {
-    StandardEffectRackPointer pRack(new StandardEffectRack(
-        m_pEffectsManager, this, m_standardEffectRacks.size()));
-    m_standardEffectRacks.append(pRack);
-    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
-    return pRack;
-}
-
-StandardEffectRackPointer EffectChainManager::getStandardEffectRack(int i) {
-    if (i < 0 || i >= m_standardEffectRacks.size()) {
-        return StandardEffectRackPointer();
-    }
-    return m_standardEffectRacks[i];
-}
-
-EqualizerRackPointer EffectChainManager::addEqualizerRack() {
-    EqualizerRackPointer pRack(new EqualizerRack(
-            m_pEffectsManager, this, m_equalizerEffectRacks.size()));
-    m_equalizerEffectRacks.append(pRack);
-    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
-    return pRack;
-}
-
-EqualizerRackPointer EffectChainManager::getEqualizerRack(int i) {
-    if (i < 0 || i >= m_equalizerEffectRacks.size()) {
-        return EqualizerRackPointer();
-    }
-    return m_equalizerEffectRacks[i];
-}
-
-QuickEffectRackPointer EffectChainManager::addQuickEffectRack() {
-    QuickEffectRackPointer pRack(new QuickEffectRack(
-        m_pEffectsManager, this, m_quickEffectRacks.size()));
-    m_quickEffectRacks.append(pRack);
-    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
-    return pRack;
-}
-
-QuickEffectRackPointer EffectChainManager::getQuickEffectRack(int i) {
-    if (i < 0 || i >= m_quickEffectRacks.size()) {
-        return QuickEffectRackPointer();
-    }
-    return m_quickEffectRacks[i];
-}
-
-OutputEffectRackPointer EffectChainManager::addOutputsEffectRack() {
-    OutputEffectRackPointer pRack(new OutputEffectRack(
-        m_pEffectsManager, this));
-    m_pOutputEffectRack = pRack;
-    m_effectRacksByGroup.insert(pRack->getGroup(), pRack);
-    return m_pOutputEffectRack;
-}
-
-OutputEffectRackPointer EffectChainManager::getMasterEffectRack() {
-    return m_pOutputEffectRack;
-}
-
-EffectRackPointer EffectChainManager::getEffectRack(const QString& group) {
-    return m_effectRacksByGroup.value(group);
-}
-
-void EffectChainManager::addEffectChain(EffectChainSlotPointer pEffectChainSlot) {
-    if (pEffectChainSlot) {
-        m_effectChainSlots.append(pEffectChainSlot);
-    }
-}
-
-void EffectChainManager::removeEffectChain(EffectChainSlotPointer pEffectChainSlot) {
-    if (pEffectChainSlot) {
-        m_effectChainSlots.removeAll(pEffectChainSlot);
-    }
-}
-
-EffectChainSlotPointer EffectChainManager::getNextEffectChain(EffectChainSlotPointer pEffectChainSlot) {
-    if (m_effectChainSlots.isEmpty())
-        return EffectChainSlotPointer();
-
-    if (!pEffectChainSlot) {
-        return m_effectChainSlots[0];
-    }
-
-    int indexOf = m_effectChainSlots.lastIndexOf(pEffectChainSlot);
-    if (indexOf == -1) {
-        qWarning() << debugString() << "WARNING: getNextEffectChain called for an unmanaged EffectChain";
-        return m_effectChainSlots[0];
-    }
-
-    return m_effectChainSlots[(indexOf + 1) % m_effectChainSlots.size()];
-}
-
-EffectChainSlotPointer EffectChainManager::getPrevEffectChain(EffectChainSlotPointer pEffectChainSlot) {
-    if (m_effectChainSlots.isEmpty())
-        return EffectChainSlotPointer();
-
-    if (!pEffectChainSlot) {
-        return m_effectChainSlots[m_effectChainSlots.size()-1];
-    }
-
-    int indexOf = m_effectChainSlots.lastIndexOf(pEffectChainSlot);
-    if (indexOf == -1) {
-        qWarning() << debugString() << "WARNING: getPrevEffectChain called for an unmanaged EffectChain";
-        return m_effectChainSlots[m_effectChainSlots.size()-1];
-    }
-
-    return m_effectChainSlots[(indexOf - 1 + m_effectChainSlots.size()) % m_effectChainSlots.size()];
-}
-
-void EffectChainManager::refeshAllRacks() {
-    for (const auto& pRack: m_standardEffectRacks) {
-        pRack->refresh();
-    }
-    for (const auto& pRack: m_equalizerEffectRacks) {
-        pRack->refresh();
-    }
-    for (const auto& pRack: m_quickEffectRacks) {
-        pRack->refresh();
-    }
-}
-
-// NOTE(Kshitij) : Use Effect Preset in constructor
-// bool EffectChainManager::saveEffectChains() {
-//     QDomDocument doc("MixxxEffects");
-
-//     QString blank = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-//         "<" + EffectXml::Root + " schemaVersion=\"" +
-//               QString::number(EffectXml::kXmlSchemaVersion) + "\">\n"
-//         "</" + EffectXml::Root + ">\n";
-//     doc.setContent(blank);
-
-//     QDomElement rootNode = doc.documentElement();
-
-//     for (EffectRackPointer pRack : m_standardEffectRacks) {
-//         rootNode.appendChild(pRack->toXml(&doc));
-//     }
-//     // TODO? Save QuickEffects in effects.xml too, or keep stored in ConfigObjects?
-// //     foreach(EffectRackPointer pRack, m_quickEffectRacks) {
-// //         rootNode.appendChild(pRack->toXML(&doc));
-// //     }
-
-//     QDir settingsPath(m_pConfig->getSettingsPath());
-
-//     if (!settingsPath.exists()) {
-//         return false;
-//     }
-
-//     QFile file(settingsPath.absoluteFilePath("effects.xml"));
-
-//     // TODO(rryan): overwrite the right way.
-//     if (!file.open(QIODevice::Truncate | QIODevice::WriteOnly)) {
-//         return false;
-//     }
-
-//     QString effectsXml = doc.toString();
-//     file.write(effectsXml.toUtf8());
-//     file.close();
-//     return true;
-// }
-
-// NOTE(Kshitij) : Use EffectPreset to load effect chains
-// void EffectChainManager::loadEffectChains() {
-//     QDir settingsPath(m_pConfig->getSettingsPath());
-//     QFile file(settingsPath.absoluteFilePath("effects.xml"));
-//     QDomDocument doc;
-
-//     QDomElement emptyChainElement = doc.createElement(EffectXml::Chain);
-//     // Check that XML file can be opened and is valid XML
-//     if (!file.open(QIODevice::ReadOnly) || !doc.setContent(&file)) {
-//         return;
-//     }
-
-//     QDomElement root = doc.documentElement();
-//     QDomElement rackElement = XmlParse::selectElement(root, EffectXml::Rack);
-//     QDomElement chainsElement = XmlParse::selectElement(rackElement, EffectXml::ChainsRoot);
-//     QDomNodeList chainsList = chainsElement.elementsByTagName(EffectXml::Chain);
-
-//     for (int i = 0; i < chainsList.count(); ++i) {
-//         QDomNode chainNode = chainsList.at(i);
-
-//         if (chainNode.isElement()) {
-//             QDomElement chainElement = chainNode.toElement();
-//             EffectChainPointer pChain = EffectChain::createFromXml(
-//                     m_pEffectsManager, chainElement);
-//             if (pChain) { // null = ejected chains.
-//                 // Note(Kshitij) : Add the effect chain to standardEffectRack
-//                 EffectChainSlotPointer pChainSlot = getStandardEffectRack(0)->getEffectChainSlot(i);
-//                 if (pChainSlot) {
-//                     pChainSlot->loadEffectChainToSlot(pChain);
-//                     pChainSlot->loadChainSlotFromXml(chainElement);
-//                     pChain->addToEngine(getStandardEffectRack(0)->getEngineEffectRack(), i);
-//                     pChain->updateEngineState();
-//                 }
-//             }
-//         }
-//     }
-// }
-
-bool EffectChainManager::isAdoptMetaknobValueEnabled() const {
-    return m_pConfig->getValue(ConfigKey("[Effects]", "AdoptMetaknobValue"), true);
-}

src/effects/effectchainmanager.h
@@ -1,89 +1,0 @@
-#ifndef EFFECTCHAINMANAGER_H
-#define EFFECTCHAINMANAGER_H
-
-#include <QObject>
-#include <QList>
-#include <QHash>
-
-#include "preferences/usersettings.h"
-#include "effects/effectrack.h"
-#include "engine/channelhandle.h"
-#include "util/class.h"
-#include "util/xml.h"
-
-class EffectsManager;
-
-// A class for keeping track of all the user's EffectChains. Eventually will
-// serialize/deserialize the EffectChains from storage but for Effects v1 we are
-// hard-coding the available chains.
-class EffectChainManager : public QObject {
-    Q_OBJECT
-  public:
-    EffectChainManager(UserSettingsPointer pConfig,
-                       EffectsManager* pEffectsManager);
-    virtual ~EffectChainManager();
-
-    void registerInputChannel(const ChannelHandleAndGroup& handle_group);
-    const QSet<ChannelHandleAndGroup>& registeredInputChannels() const {
-        return m_registeredInputChannels;
-    }
-
-    void registerOutputChannel(const ChannelHandleAndGroup& handle_group);
-    const QSet<ChannelHandleAndGroup>& registeredOutputChannels() const {
-        return m_registeredOutputChannels;
-    }
-
-    StandardEffectRackPointer addStandardEffectRack();
-    StandardEffectRackPointer getStandardEffectRack(int rack);
-
-    EqualizerRackPointer addEqualizerRack();
-    EqualizerRackPointer getEqualizerRack(int rack);
-
-    QuickEffectRackPointer addQuickEffectRack();
-    QuickEffectRackPointer getQuickEffectRack(int rack);
-
-    OutputEffectRackPointer addOutputsEffectRack();
-    OutputEffectRackPointer getMasterEffectRack();
-
-    EffectRackPointer getEffectRack(const QString& group);
-
-    void addEffectChain(EffectChainSlotPointer pEffectChainSlot);
-    void removeEffectChain(EffectChainSlotPointer pEffectChainSlot);
-
-    // To support cycling through effect chains, there is a global ordering of
-    // chains. These methods allow you to get the next or previous chain given
-    // your current chain.
-    // TODO(rryan): Prevent double-loading of a chain into a slot?
-    EffectChainSlotPointer getNextEffectChain(EffectChainSlotPointer pEffectChainSlot);
-    EffectChainSlotPointer getPrevEffectChain(EffectChainSlotPointer pEffectChainSlot);
-
-    // NOTE(Kshitij) : New functions for saving and loading
-    // bool saveEffectChains();
-    // void loadEffectChains();
-
-    // Reloads all effect to the slots to update parameter assignments
-    void refeshAllRacks();
-
-    static const int kNumStandardEffectChains = 4;
-
-    bool isAdoptMetaknobValueEnabled() const;
-
-  private:
-    QString debugString() const {
-        return "EffectChainManager";
-    }
-
-    UserSettingsPointer m_pConfig;
-    EffectsManager* m_pEffectsManager;
-    QList<StandardEffectRackPointer> m_standardEffectRacks;
-    QList<EqualizerRackPointer> m_equalizerEffectRacks;
-    QList<QuickEffectRackPointer> m_quickEffectRacks;
-    OutputEffectRackPointer m_pOutputEffectRack;
-    QHash<QString, EffectRackPointer> m_effectRacksByGroup;
-    QList<EffectChainSlotPointer> m_effectChainSlots;
-    QSet<ChannelHandleAndGroup> m_registeredInputChannels;
-    QSet<ChannelHandleAndGroup> m_registeredOutputChannels;
-    DISALLOW_COPY_AND_ASSIGN(EffectChainManager);
-};
-
-#endif /* EFFECTCHAINMANAGER_H */

src/effects/effectchainslot.cpp
@@ -453,6 +453,7 @@ void EffectChainSlot::enableForInputChannel(const ChannelHandleAndGroup& handle_
           mixxx::AudioSignal::SampleRate(96000),
           MAX_BUFFER_LEN / mixxx::kEngineChannelCount);
 
+    // TODO: Simplify by defining a method to create an EffectState for the input channel
     for (int i = 0; i < m_effects.size(); ++i) {
         auto& statesMap = (*pEffectStatesMapArray)[i];
         if (m_effects[i] != nullptr) {

src/effects/effectchainslot.h
@@ -7,13 +7,12 @@
 #include <QSignalMapper>
 #include <QDomDocument>
 
+#include "control/controlobject.h"
 #include "effects/defs.h"
 #include "effects/effect.h"
 #include "engine/channelhandle.h"
 #include "util/class.h"
 
-
-class ControlObject;
 class ControlPushButton;
 class ControlEncoder;
 class EffectChainSlot;

src/effects/effectprocessor.h
@@ -1,4 +1,3 @@
-
 #ifndef EFFECTPROCESSOR_H
 #define EFFECTPROCESSOR_H
 
@@ -13,7 +12,6 @@
 #include "engine/effects/groupfeaturestate.h"
 #include "engine/effects/message.h"
 #include "engine/channelhandle.h"
-#include "effects/effectsmanager.h"
 
 class EngineEffect;
 
@@ -57,7 +55,7 @@ class EffectProcessor {
     // Called from main thread to avoid allocating memory in the audio callback thread
     virtual void initialize(
             const QSet<ChannelHandleAndGroup>& activeInputChannels,
-            EffectsManager* pEffectsManager,
+            const QSet<ChannelHandleAndGroup>& registeredOutputChannels,
             const mixxx::EngineParameters& bufferParameters) = 0;
     virtual EffectState* createState(const mixxx::EngineParameters& bufferParameters) = 0;
     virtual bool loadStatesForInputChannel(const ChannelHandle* inputChannel,
@@ -91,8 +89,7 @@ class EffectProcessor {
 template <typename EffectSpecificState>
 class EffectProcessorImpl : public EffectProcessor {
   public:
-    EffectProcessorImpl()
-      : m_pEffectsManager(nullptr) {
+    EffectProcessorImpl() {
     }
     // Subclasses should not implement their own destructor. All state should
     // be stored in the EffectState subclass, not the EffectProcessorImpl subclass.
@@ -159,16 +156,17 @@ class EffectProcessorImpl : public EffectProcessor {
     }
 
     void initialize(const QSet<ChannelHandleAndGroup>& activeInputChannels,
-            EffectsManager* pEffectsManager,
+            const QSet<ChannelHandleAndGroup>& registeredOutputChannels,
             const mixxx::EngineParameters& bufferParameters) final {
+        m_registeredOutputChannels = registeredOutputChannels;
+
         for (const ChannelHandleAndGroup& inputChannel : activeInputChannels) {
             if (kEffectDebugOutput) {
                 qDebug() << this << "EffectProcessorImpl::initialize allocating "
                             "EffectStates for input" << inputChannel;
             }
             ChannelHandleMap<EffectSpecificState*> outputChannelMap;
-            for (const ChannelHandleAndGroup& outputChannel :
-                    pEffectsManager->registeredOutputChannels()) {
+            for (const ChannelHandleAndGroup& outputChannel : m_registeredOutputChannels) {
                 outputChannelMap.insert(outputChannel.handle(),
                         createSpecificState(bufferParameters));
                 if (kEffectDebugOutput) {
@@ -178,8 +176,6 @@ class EffectProcessorImpl : public EffectProcessor {
             }
             m_channelStateMatrix.insert(inputChannel.handle(), outputChannelMap);
         }
-        m_pEffectsManager = pEffectsManager;
-        DEBUG_ASSERT(m_pEffectsManager != nullptr);
     };
 
     EffectState* createState(const mixxx::EngineParameters& bufferParameters) final {
@@ -217,8 +213,8 @@ class EffectProcessorImpl : public EffectProcessor {
               }
           }
 
-          for (const ChannelHandleAndGroup& outputChannel :
-                  m_pEffectsManager->registeredOutputChannels()) {
+          QSet<ChannelHandleAndGroup> receivedOutputChannels = m_registeredOutputChannels;
+          for (const ChannelHandleAndGroup& outputChannel : m_registeredOutputChannels) {
               if (kEffectDebugOutput) {
                   qDebug() << "EffectProcessorImpl::loadStatesForInputChannel"
                            << this << "output" << outputChannel;
@@ -230,7 +226,11 @@ class EffectProcessorImpl : public EffectProcessor {
                     return false;
               }
               effectSpecificStatesMap.insert(outputChannel.handle(), pState);
+              receivedOutputChannels.insert(outputChannel);
           }
+          // Output channels are hardcoded in EngineMaster and should not
+          // be registered after Mixxx initializes.
+          DEBUG_ASSERT(receivedOutputChannels == m_registeredOutputChannels);
           return true;
     };
 
@@ -272,7 +272,7 @@ class EffectProcessorImpl : public EffectProcessor {
         return pState;
     };
 
-    EffectsManager* m_pEffectsManager;
+    QSet<ChannelHandleAndGroup> m_registeredOutputChannels;
     ChannelHandleMap<ChannelHandleMap<EffectSpecificState*>> m_channelStateMatrix;
 };
 

commit ff9ead0d582d9d4a7252a34c4e03444865b73629

fix crash on shutdown when an LV2 effect was loaded
The crash was introduced in
fded4f8d5b483681f19bbd0943baa806c8c8aad7

src/effects/effectsmanager.cpp
@@ -50,9 +50,18 @@ EffectsManager::~EffectsManager() {
 
     // NOTE(Kshitij) : Use new functions for saving XML files
     // saveEffectChains();
-    // This must be done here, since the engineRacks are deleted via
-    // the queue
+
+    // The EffectChainSlots must be deleted before the EffectsBackends in case
+    // there is an LV2 effect currently loaded.
+    // ~LV2GroupState calls lilv_instance_free, which will segfault if called
+    // after ~LV2Backend calls lilv_world_free.
+    m_equalizerEffectChainSlots.clear();
+    m_quickEffectChainSlots.clear();
+    m_standardEffectChainSlots.clear();
+    m_outputEffectChainSlot.clear();
+    m_effectChainSlotsByGroup.clear();
     processEffectsResponses();
+
     while (!m_effectsBackends.isEmpty()) {
         EffectsBackend* pBackend = m_effectsBackends.takeLast();
         delete pBackend;